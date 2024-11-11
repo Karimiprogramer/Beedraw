fbd8becddedede6a74248b06877d4b39f6c6cb4b
Adnan <a9nvnMobile@gmail.com>
2024-11-11 20:49:38 +0330

    userTbl and FriendsTbl

--- BeeDraw.Database/Contexts/ApplicationDbContext.cs
+++ BeeDraw.Database/Contexts/ApplicationDbContext.cs
@@ -1,12 +1,17 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using BeeDraw.Database.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace BeeDraw.Database.Contexts
 {
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
     {
 
+        public DbSet<UserTbl> UserTbl { get; set; }
+        public DbSet<FriendshipTbl> FriendshipTbl { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<UserTbl>().HasQueryFilter(u => !u.IsDeleted);
+
         }
     }
 }
---
+++ BeeDraw.Database/Models/UserTbl.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using BeeDraw.Database.SeedWorks.Base;
+
+namespace BeeDraw.Database.Models;
+
+public class UserTbl:EntityBase
+{
+    [Required]
+    public long TelegramUserId { get; set; }  
+    
+    [MaxLength(50)]
+    public required string Username { get; set; }
+
+    [MaxLength(100)]
+    public required string FirstName { get; set; }
+
+    [MaxLength(100)]
+    public required string LastName { get; set; }
+
+    [MaxLength(10)]
+    public required string LanguageCode { get; set; }
+
+    public required string PhoneNumber { get; set; }
+
+    
+    
+    public DateTime DateJoined { get; set; } = DateTime.UtcNow;
+
+    public bool IsBlocked { get; set; } = false;
+    public bool IsDeleted { get; set; } = false;
+    public bool IsBot { get; set; }
+
+    
+    public List<FriendshipTbl> Friends { get; set; } = [];
+
+}
